b218400e71c1f1a38a0c50bb2e392964ad2c2069
Dineth Senura <100292025+DinethSenura@users.noreply.github.com>
2025-01-08 20:43:16 +0530

    dropdown update

--- src/components/Common/Header/Header.jsx
+++ src/components/Common/Header/Header.jsx
@@ -35,30 +35,97 @@ const Header = () => {
                 HOME
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink to="/about" className="nav-link" activeClassName="active">
+
+            {/* About Dropdown */}
+            <li className="nav-item dropdown">
+              <NavLink
+                to="/about"
+                className="nav-link dropdown-toggle"
+                id="aboutDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 ABOUT
               </NavLink>
+              <ul className="dropdown-menu" aria-labelledby="aboutDropdown">
+                <li><NavLink to="/about/history" className="dropdown-item">History</NavLink></li>
+                <li><NavLink to="/about/team" className="dropdown-item">Team</NavLink></li>
+                <li><NavLink to="/about/mission" className="dropdown-item">Mission</NavLink></li>
+              </ul>
             </li>
-            <li className="nav-item">
-              <NavLink to="/ports" className="nav-link" activeClassName="active">
+
+            {/* Ports Dropdown */}
+            <li className="nav-item dropdown">
+              <NavLink
+                to="/ports"
+                className="nav-link dropdown-toggle"
+                id="portsDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 PORTS
               </NavLink>
+              <ul className="dropdown-menu" aria-labelledby="portsDropdown">
+                <li><NavLink to="/ports/terminal1" className="dropdown-item">Terminal 1</NavLink></li>
+                <li><NavLink to="/ports/terminal2" className="dropdown-item">Terminal 2</NavLink></li>
+                <li><NavLink to="/ports/terminal3" className="dropdown-item">Terminal 3</NavLink></li>
+              </ul>
             </li>
-            <li className="nav-item">
-              <NavLink to="/projects" className="nav-link" activeClassName="active">
+
+            {/* Projects Dropdown */}
+            <li className="nav-item dropdown">
+              <NavLink
+                to="/projects"
+                className="nav-link dropdown-toggle"
+                id="projectsDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 PROJECTS
               </NavLink>
+              <ul className="dropdown-menu" aria-labelledby="projectsDropdown">
+                <li><NavLink to="/projects/ongoing" className="dropdown-item">Ongoing</NavLink></li>
+                <li><NavLink to="/projects/completed" className="dropdown-item">Completed</NavLink></li>
+              </ul>
             </li>
-            <li className="nav-item">
-              <NavLink to="/procurement" className="nav-link" activeClassName="active">
+
+            {/* Procurement */}
+            <li className="nav-item dropdown">
+              <NavLink
+                to="/procurement"
+                className="nav-link dropdown-toggle"
+                id="procurementDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 PROCUREMENT
               </NavLink>
+              <ul className="dropdown-menu" aria-labelledby="procurementDropdown">
+                <li><NavLink to="/procurement/tenders" className="dropdown-item">Tenders</NavLink></li>
+                <li><NavLink to="/procurement/bids" className="dropdown-item">Bids</NavLink></li>
+              </ul>
             </li>
-            <li className="nav-item">
-              <NavLink to="/contact" className="nav-link" activeClassName="active">
+
+            {/* Contact */}
+            <li className="nav-item dropdown">
+              <NavLink
+                to="/contact"
+                className="nav-link dropdown-toggle"
+                id="contactDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 CONTACT
               </NavLink>
+              <ul className="dropdown-menu" aria-labelledby="contactDropdown">
+                <li><NavLink to="/contact/support" className="dropdown-item">Support</NavLink></li>
+                <li><NavLink to="/contact/feedback" className="dropdown-item">Feedback</NavLink></li>
+              </ul>
             </li>
           </ul>
         </div>
