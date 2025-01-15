3d3dde1ee95e149cb118859564ce1c56b8661289
Dishni N Rathnasekara <disninimeshika2000@gmail.com>
2025-01-15 14:33:56 +0530

    News

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,52 +1,93 @@
-import React from 'react';
-import '../News/news.css'; // Include CSS for styling
+import React from "react";
+import "./news.css";
 import news1 from '../../assets/images/News/News_1.jpg'; // Replace with actual image paths
 import news2 from '../../assets/images/News/News_2.jpg';
 import news3 from '../../assets/images/News/News_3.jpg';
 
-const News = () => {
+
+
+/*const newsData = [
+  {
+    id: 1,
+    title: "Port of Colombo Achieves Milestone",
+    description: "With ongoing developments we are confident in the Port of Colombo's evolution as the hub of South Asia” – Keith D. Bernard, Chairman SLPA",
+    image: "src\assets\images (1).jpg",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Sustainability in Maritime Sector",
+    description: "Green energy initiatives are reshaping the maritime industry...",
+    image: "src\assets\images (2).jpg",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "World Maritime Day Highlights",
+    description: "Celebrating innovation and safety in the maritime industry...",
+    image: "src\assets\images (3).jpg",
+    link: "#",
+  },
+];*/
+
+const LatestNews = () => {
   return (
-    <div className="latest-news1">
-      <h2 className="news-title1">Latest News</h2>
-      <div className="news-container1">
-        {/* News 11 */}
-        <div className="news-item_11">
-          <img src={news1} alt="News 1" className="news-image_11" />
-          <p className="news-description1">
-            “With ongoing developments we are confident in the Port of Colombo's evolution as the hub of South Asia” – Keith D. Bernard, Chairman SLPA
-          </p>
-          <button className="news-button1">Read more</button>
-        </div>
-
-        {/* News 31 */}
-        <div className='right_side'>
-          <div>
-        <div className="news-item_31">
-          <img src={news3} alt="News 3" className="news-image_31" />
-          <div className='image23'>
-          <p className="news-description1">
-            Steering Towards a Safer Future: World Maritime Day 2024
-          </p>
-          <button className="news-button1">Read more</button>
+    /*<section className="latest-news">
+      <h2 className="section-title">Latest News</h2>
+      <div className="news-container">
+        {newsData.map((news) => (
+          <div className="news-item" key={news.id}>
+            <img src={news.image} alt={news.title} className="news-image" />
+            <div className="news-content">
+              <h3 className="news-title">{news.title}</h3>
+              <p className="news-description">{news.description}</p>
+              <a href={news.link} className="news-button">
+                Read More
+              </a>
+            </div>
           </div>
-        </div>
-        </div>
-
-        {/* News 21 */}
-        <div className="news-item_21">
-          <img src={news2} alt="News 2" className="news-image_21" />
-          <div className='image23'>
-          <p className="news-description1">
-            “The optimum usage of energy is a mandatory requirement” – Eng. Ganaka Hemachandra, Managing Director, SLPA
-          </p>
-          <button className="news-button1">Read more</button>
-        </div>
-        </div>
-        </div>
+        ))}
       </div>
-      <button className="more-news-button1">MORE LATEST NEWS →</button>
-    </div>
+    </section>*/
+    <div className="latest-news">
+          <h2 className="section-title">Latest News</h2>
+          <div className="news-container">
+            
+            <div className="news-item">
+                
+
+                
+              <img src={news1} alt="" className="news-image" />
+              <p className="news-description">
+                “With ongoing developments we are confident in the Port of Colombo's evolution as the hub of South Asia” – Keith D. Bernard, Chairman SLPA
+              </p>
+              <button className="news-button">Read more</button>
+            </div>
+            
+            <div className="news-item">
+              <img src={news2} alt="News 2" className="news-image" />
+              <p className="news-description">
+                “The optimum usage of energy is a mandatory requirement” – Eng. Ganaka Hemachandra, Managing Director, SLPA
+              </p>
+              <button className="news-button">Read more</button>
+            </div>
+    
+            
+            <div className="news-item">
+              <img src={news3} alt="News 3" className="news-image" />
+              <p className="news-description">
+                Steering Towards a Safer Future: World Maritime Day 2024
+              </p>
+              <button className="news-button">Read more</button>
+            </div>
+                
+          </div>
+    
+          
+          <button className="more-news-button">MORE LATEST NEWS →</button>
+        </div>
+      
   );
 };
 
-export default News;
+export default LatestNews;
