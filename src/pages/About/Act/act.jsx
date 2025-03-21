c4d32e9206dd25e8ebea4431492f586b62ca93e5
Dishni N Rathnasekara <disninimeshika2000@gmail.com>
2025-03-21 10:59:57 +0530

    update act bod procedures

--- src/pages/About/Act/act.jsx
+++ src/pages/About/Act/act.jsx
@@ -1,30 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import DOMPurify from 'dompurify';
 
-const berthingprogramme = () => {
-  return (
-    <div>
-     
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus laoreet, nunc a tristique facilisis, mi nisi dapibus dui, eget tempor metus elit nec sapien. Aliquam erat volutpat. Proin euismod, arcu non scelerisque aliquet, risus purus scelerisque risus, eget ullamcorper libero risus eget arcu. Integer volutpat, felis at vehicula varius, ante leo tincidunt arcu, vel varius tortor sem et mi. Integer tincidunt suscipit tortor id iaculis. Suspendisse tincidunt, purus vel laoreet feugiat, ligula risus pharetra lorem, at efficitur ipsum risus vel enim. Quisque vel libero sed nulla aliquet pharetra. In rhoncus urna a ligula venenatis, sed tempor turpis venenatis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+const ActOfSLPA = () => {
+  const [htmlContent, setHtmlContent] = useState('');
 
-          Curabitur euismod nunc sed orci gravida, non ullamcorper nunc venenatis. Nam ut varius nunc, ac tempor nulla. Nulla facilisi. Morbi sit amet ante nunc. Donec vestibulum purus eu enim gravida, ut auctor sapien tempus. Nulla rutrum augue at lectus vehicula tristique. Mauris quis turpis et arcu feugiat finibus. Aenean nec enim eget libero iaculis placerat. Proin posuere orci nisl, vitae tempus purus efficitur vitae. Nulla malesuada euismod arcu, sit amet ultricies leo lacinia id. Donec efficitur, tortor sit amet aliquet posuere, nisi mi lacinia magna, nec bibendum ante velit vel mauris. Vivamus cursus malesuada eros, vel blandit mi malesuada sit amet. Ut faucibus feugiat ex, non elementum magna feugiat non. Morbi non velit risus.
+  useEffect(() => {
+    // Directly set the provided HTML content.
+    const apiData = {
+      title: "ACT of SLPA",
+      sub_title: "",
+      image: "article_image_2019_06_04_1559624237.jpg",
+      content: `<p style="text-align: justify;"><strong>AN ACT</strong>&nbsp;to provide for the establishment of the Sri Lanka Ports Authority to develop, maintain, operate and provide port and other services in the ports of Colombo, Galle and Trincomalee and any other port as may be declared hereafter by the minister by order, to be a port to which this Act shall apply; for the exercise, performance and discharge by that authority of the powers, duties and functions of the Port Commissioner, the Port (Cargo) Corporation, and the Port Tally and Protective Services Corporation and such other powers, duties and functions as may be conferred, assigned or imposed on that authority by law; for matters in relation to the officers and servants, property, rights, obligations and liabilities of the Port (Cargo) Corporation and the Port Tally and Protective Services Corporation and the public officers of, the property held by, and the rights. obligations and liabilities of, the department of the Port Commissioner; for the repeal of the Port of Colombo (Administration) act, the Port (Cargo) Corporation Act and the port tally and protective services corporation act; for the modification of certain laws in their application to the ports of Colombo, Galle and Trincomalee and any other port as may be declared hereafter by the Minister by order, to be a port to which this act shall apply; and for connected matters.</p>
 
-          Donec id mauris interdum, cursus ligula in, aliquam sapien. In euismod orci sit amet mauris malesuada sollicitudin. Sed et ligula ipsum. Fusce volutpat ac odio nec sodales. In pretium libero id arcu dapibus, nec volutpat eros condimentum. Curabitur at sollicitudin nunc. Etiam malesuada sem non risus dignissim, ut vehicula libero interdum. Ut id suscipit orci. Aliquam tincidunt felis metus, nec tincidunt nulla interdum eu. Integer et justo eros. Etiam euismod erat vitae arcu scelerisque, at ullamcorper ipsum auctor. Nam suscipit, enim ac egestas feugiat, justo orci viverra felis, ac feugiat orci orci sit amet ante.
+<p>[Date of Commencement:&nbsp;1st&nbsp;August, 1979]</p>`
+    };
 
-          Vivamus non lorem leo. Fusce sit amet ex sed nisi sodales tincidunt. Phasellus non interdum magna. Aenean maximus felis at mauris feugiat ultricies. Curabitur vestibulum augue id cursus scelerisque. Nullam viverra felis in eros auctor tempus. Integer in justo eu felis tincidunt auctor ut vel velit. Suspendisse potenti. Quisque vitae massa ut risus tincidunt varius non vel ante. Proin ut ante sit amet neque cursus feugiat. Curabitur tempor lorem vel libero efficitur, eu interdum purus vehicula. Donec mollis lacus sit amet ante tincidunt tincidunt. Sed pretium nunc id tortor tempor, id dapibus felis tincidunt.
+    setHtmlContent(DOMPurify.sanitize(apiData.content));
+  }, []);
 
-          Maecenas egestas laoreet est, sit amet venenatis dui blandit vel. Fusce sollicitudin ligula eu lectus maximus, nec tempor odio vulputate. Fusce imperdiet metus ac turpis scelerisque auctor. Aliquam non ex quis nulla sodales tincidunt. Nulla viverra purus ac urna volutpat, non dapibus neque eleifend. Vivamus euismod arcu sed dolor consequat, at luctus ante pharetra. Ut aliquet placerat turpis id dapibus. Sed et ante tortor. Donec elementum purus id lorem pellentesque, ac tempus sem sollicitudin. Fusce cursus sapien id ipsum malesuada, id maximus magna euismod. Phasellus eu ante ut leo lobortis consequat. Curabitur rhoncus volutpat eros eu placerat.
+  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+};
 
-          Nulla viverra venenatis dui, non dictum orci cursus vitae. Ut felis dui, tincidunt ut volutpat ac, luctus non est. Ut cursus velit dui, id pretium dui facilisis id. Aliquam tincidunt euismod ipsum sit amet fermentum. Donec ut ante et neque pharetra sodales non id ligula. Integer scelerisque dui orci, vitae mollis lacus gravida a. Vivamus sodales, dui et venenatis placerat, risus ligula malesuada neque, sed pretium nulla enim ac elit. Vivamus pretium velit lorem, ac condimentum lorem fringilla in. Cras sit amet felis urna. Donec id velit malesuada, maximus urna sed, fermentum ante. Etiam posuere varius augue, ac convallis lorem ultricies ac.
-
-          Sed ut dolor magna. Curabitur a felis vulputate, malesuada arcu id, consequat metus. Nunc lacinia nunc eget eros viverra, ut hendrerit dui tincidunt. Morbi et mollis eros. Integer posuere neque lectus, vel efficitur turpis eleifend in. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer finibus felis sed vestibulum tempor. Nullam vel erat nec sapien viverra gravida ac ut lorem. Proin convallis nisi et libero tincidunt, vitae auctor erat interdum. Curabitur at arcu eget dolor malesuada mollis sed id risus. In ac nisi ac nunc aliquet suscipit. Integer ac varius lorem. Vivamus fermentum, arcu vitae elementum lacinia, dui erat feugiat ligula, ac posuere arcu sapien nec purus.
-
-          Nam interdum velit ut risus laoreet sollicitudin. Etiam vestibulum in nunc at hendrerit. Integer molestie arcu non purus condimentum ultricies. Morbi volutpat quam sit amet dolor rhoncus, sit amet facilisis urna volutpat. Ut laoreet eros orci, ut maximus sapien tempus nec. Nam vehicula dolor vel ante tincidunt, non rhoncus felis volutpat. Ut vel augue magna. Donec suscipit auctor felis, euismod tempus felis iaculis eu. Sed auctor urna in fringilla pharetra. Nunc facilisis dolor at justo scelerisque, et convallis odio tincidunt. Sed efficitur lorem sit amet odio finibus, ut dictum nunc tincidunt. Donec eget libero justo.
-        </p>
-      </div>
-    </div>
-  )
-}
-
-export default berthingprogramme
+export default ActOfSLPA;
