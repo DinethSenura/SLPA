c5986e84d8c588c1d1aff9cb90ed222bdfa1ba0d
Dineth Senura <100292025+DinethSenura@users.noreply.github.com>
2025-05-20 09:28:49 +0530

    brething more button

--- src/components/BirthingProgram/Birthingprogram.jsx
+++ src/components/BirthingProgram/Birthingprogram.jsx
@@ -59,7 +59,10 @@ const Esystem = () => {
               </tbody>
             </table>
           </div>
-          <a href="Brething-program">More</a>
+          <a href="http://www.slpa.lk/berthing-programme/" target="_blank" rel="noopener noreferrer"
+>
+  More
+</a>
         </div>
       )}
     </div>
